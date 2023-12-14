206533e19b9f152f1567d4920128f33cee4dc7a2
chenjj <2548077705@qq.com>
2023-12-14 15:29:57 +0800

    a initial springboot project

---
+++ src/test/java/com/example/malllearning/MallLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.malllearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
